f4484e8146c5d638e2cc447a8c99e19090554c77
EzyhB <ezyhbmail@gmail.com>
2022-02-26 18:40:33 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: `new`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-emotion", "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
